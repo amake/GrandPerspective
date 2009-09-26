a54c42c1a5a0c82580c4a393cdf997f969b34f9d
eriban <eriban@b82a47f9-25c6-438f-ab64-01e370b75a6b>
2009-09-26 17:27:11 +0000

    Tagging Version 1.0 of the scripts.
    git-svn-id: svn://svn.code.sf.net/p/grandperspectiv/code/GrandPerspective/tags/release-1_0@1159 b82a47f9-25c6-438f-ab64-01e370b75a6b

---
+++ bin/publish-app.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ $# -ne "3" ]
+then
+  echo "Script requires three arguments."
+  exit -1
+fi
+
+SOURCE_TGZ=$1
+APP_PATH=$2
+DEST_PATH=$3
+
+if [[ "$TEMP_DIR" == "" || ! -d $TEMP_DIR ]]
+then
+  echo "TEMP_DIR not set (correctly)."
+  exit -1
+fi
+
+TEMP_PUBLISH_DIR=`mktemp -d ${TEMP_DIR}/publish-XXXXXX` || exit -1
+
+echo "Temporary output to" $TEMP_PUBLISH_DIR
+
+TMP=${SOURCE_TGZ##*/}
+TMP=${TMP#GrandPerspective-}
+VERSION_ID=${TMP%-src.tgz}
+
+VERSION=$( echo $VERSION_ID | tr "_" "." )
+
+BIN_DIR=`pwd`
+
+OUTER_DIR=GrandPerspective-${VERSION_ID}
+OUTER_DIR_PATH=$TEMP_PUBLISH_DIR/$OUTER_DIR
+OUT_DMG_FILE=GrandPerspective-${VERSION_ID}.dmg
+VOLUME_NAME="GrandPerspective ${VERSION}"
+
+echo "Extracting source archive"
+tar xzf $SOURCE_TGZ -C $TEMP_PUBLISH_DIR
+
+rm -rf $OUTER_DIR_PATH/src
+
+echo "Copying application"
+mkdir $OUTER_DIR_PATH/GrandPerspective.app
+tar cf - -C ${APP_PATH} --exclude "classes.nib" --exclude "info.nib" --exclude "nl.lproj" . | tar xf - -C $OUTER_DIR_PATH/GrandPerspective.app
+
+
+# Create application DMG file.
+#
+pushd $DEST_PATH > /dev/null
+$BIN_DIR/buildDMG.pl -dmgName ${OUT_DMG_FILE%.dmg} -volName "${VOLUME_NAME}" -volSize 2 -compressionLevel 9 $OUTER_DIR_PATH/*.txt $OUTER_DIR_PATH/GrandPerspective.app
+popd > /dev/null
+
+rm -rf $TEMP_PUBLISH_DIR
---
+++ bin/publish-local.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# Creates the GrandPerspective DMG release file with localized resources.
+# It assumes that the disk image for the basic release file is mounted, as
+# it copies the application directly from it. Release text files and the
+# localized resources are taken from the SVN repository.
+
+VERSION="1.0"
+REV=1073
+NL_REV=1073
+
+if [ $# -ne "1" ]
+then
+  echo "Script requires one argument."
+  exit -1
+fi
+
+DEST_PATH=$1
+
+if [ "$GP_SVN_URL" == "" ]
+then
+  echo "GP_SVN_URL not set correctly."
+  exit -1
+fi
+
+if [[ "$TEMP_DIR" == "" || ! -d $TEMP_DIR ]]
+then
+  echo "TEMP_DIR not set (correctly)."
+  exit -1
+fi
+
+TEMP_PUBLISH_DIR=`mktemp -d ${TEMP_DIR}/publish-local-XXXXXX` || exit -1
+
+echo "Temporary output to" $TEMP_PUBLISH_DIR
+
+VERSION_ID=$( echo $VERSION | tr ". " "_-" )
+
+APP_FOLDER=GrandPerspective.app
+HELP_FOLDER=GrandPerspectiveHelp
+RESOURCES_PATH=$TEMP_PUBLISH_DIR/$APP_FOLDER/Contents/Resources
+
+OUT_DMG_FILE=GrandPerspective-${VERSION_ID}-local.dmg
+VOLUME_NAME="GrandPerspective ${VERSION} (Localized)"
+
+echo "Copying basic application."
+tar cf - -C "/Volumes/GrandPerspective ${VERSION}" $APP_FOLDER | tar xf - -C $TEMP_PUBLISH_DIR
+
+echo "Exporting text files."
+./export-docs.sh trunk/docs $REV $TEMP_PUBLISH_DIR $VERSION $VERSION_ID
+
+echo "Exporting credits."
+svn export -q -r $REV $GP_SVN_URL/trunk/code/Credits.rtf $RESOURCES_PATH/Credits.rtf
+
+echo "Exporting nl resources."
+svn export -q -r $NL_REV $GP_SVN_URL/trunk/code/nl.lproj $RESOURCES_PATH/nl.lproj
+
+echo "Exporting nl help documentation."
+./export-help.sh $NL_REV $RESOURCES_PATH nl
+
+for loc in ja fr es
+do
+  echo "Exporting" $loc "resources."
+  svn export -q -r $REV $GP_SVN_URL/trunk/locs/${loc}.lproj $RESOURCES_PATH/${loc}.lproj
+done
+
+echo "Updating nl help index."
+./update-help-index.sh $RESOURCES_PATH/nl.lproj/$HELP_FOLDER European
+
+echo "Updating fr help index."
+./update-help-index.sh $RESOURCES_PATH/fr.lproj/$HELP_FOLDER European
+
+echo "Updating es help index."
+./update-help-index.sh $RESOURCES_PATH/es.lproj/$HELP_FOLDER European
+
+# Remove unneeded nib files.
+find $RESOURCES_PATH/ \( -name classes.nib -or -name info.nib \) -delete
+
+/Users/Erwin/bin/buildDMG.pl -dmgName ${OUT_DMG_FILE%.dmg} -volName "${VOLUME_NAME}" -buildDir $DEST_PATH -volSize 4 -compressionLevel 9 ${TEMP_PUBLISH_DIR}/*.txt ${TEMP_PUBLISH_DIR}/${APP_FOLDER}
+
+rm -rf $TEMP_PUBLISH_DIR
+
