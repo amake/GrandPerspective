amake/GrandPerspective

commit 367230e15258fd03c95aa4462ad9dac3c8e50e81

Fixed warnings
Many warnings are fixed. Most (all?) of the following types:
a) Not all code paths return a value (after an assert)
b) Implicit casts of integer value
c) Unused variable

None of these did reveal bugs. Nevertheless, good to have the code cleaned up.

Wrt to the usage of integers did more than fix warnings. More generally changed declarations to better reflecting the semantics by switching to unsigned integers where appropriate.

control/FilterTestWindowControl.m
@@ -619,7 +619,7 @@
     }
     
     [sizeLowerBoundCheckBox setState: NSOnState]; 
-    [sizeLowerBoundField setIntValue: bound];
+    [sizeLowerBoundField setIntegerValue: bound];
     [sizeLowerBoundUnits selectItemAtIndex: i]; 
   }
 
@@ -635,7 +635,7 @@
     }
     
     [sizeUpperBoundCheckBox setState: NSOnState];
-    [sizeUpperBoundField setIntValue: bound];
+    [sizeUpperBoundField setIntegerValue: bound];
     [sizeUpperBoundUnits selectItemAtIndex: i];
   }
 }
@@ -708,7 +708,7 @@
   int  bytesUnit = [FileItem bytesPerKilobyte];
 
   ITEM_SIZE  lowerBound = MAX(0, [sizeLowerBoundField intValue]);
-  int  i = [sizeLowerBoundUnits indexOfSelectedItem];
+  NSUInteger  i = [sizeLowerBoundUnits indexOfSelectedItem];
   while (i-- > 0) {
     lowerBound *= bytesUnit;
   }
@@ -769,7 +769,7 @@
 
 
 - (FileItemTest *)selectiveItemTestBasedOnState:(FileItemTest *)subTest {
-  int  index = [testTargetPopUp indexOfSelectedItem];
+  NSUInteger  index = [testTargetPopUp indexOfSelectedItem];
   
   if (index == POPUP_FILES_AND_FOLDERS) { 
     return subTest;
@@ -952,8 +952,9 @@
 }
 
 - (void) removeTarget {
-  int  selectedRow = [targetsView selectedRow];
-  [matchTargets removeObjectAtIndex: selectedRow];
+  NSInteger  selectedRow = [targetsView selectedRow];
+
+  NSAssert(selectedRow >= 0, @"No row selected");
 
   if (selectedRow == [matchTargets count] && selectedRow > 0) {
     [targetsView selectRowIndexes: [NSIndexSet indexSetWithIndex: selectedRow - 1] 
@@ -1008,6 +1009,7 @@
          addTargetButton:(NSButton *)addButton
          removeTargetButton:(NSButton *)removeButton {
   NSAssert(NO, @"Use other initialiser.");
+  return nil;
 }
 
 - (id) initWithMatchModePopUpButton:(NSPopUpButton *)popUpButton
@@ -1064,7 +1066,7 @@
     return;
   }
 
-  int  newRow =  [matchTargets count];
+  NSUInteger  newRow = [matchTargets count];
   
   [matchTargets addObject: 
      NSLocalizedString( @"New match", 
@@ -1254,7 +1256,7 @@
 
 - (void) addTarget {
   NSPopUpButton  *popUp = (NSPopUpButton *)addTargetButton;
-  int  selectedIndex = [popUp indexOfSelectedItem];
+  NSInteger  selectedIndex = [popUp indexOfSelectedItem];
   if (selectedIndex <= 0) {
     return;
   }
@@ -1267,7 +1269,7 @@
   // Restore popup state
   [popUp selectItemAtIndex: POPUP_ADD_TYPE];
 
-  int  newRow =  [matchTargets count];
+  NSUInteger  newRow = [matchTargets count];
 
   [matchTargets addObject: type];
   [targetsView reloadData];

commit 5af2f4493d4c9022457e95a110a84c7e5ccd445c

Bug fix: Remove button does not do anything in Filter Test Window
Fixed regression introduced in 367230e. During refactoring, an essential statement was accidentaly removed.

control/FilterTestWindowControl.m
@@ -955,6 +955,7 @@
   NSInteger  selectedRow = [targetsView selectedRow];
 
   NSAssert(selectedRow >= 0, @"No row selected");
+  [matchTargets removeObjectAtIndex: selectedRow];
 
   if (selectedRow == [matchTargets count] && selectedRow > 0) {
     [targetsView selectRowIndexes: [NSIndexSet indexSetWithIndex: selectedRow - 1] 