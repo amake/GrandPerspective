amake/GrandPerspective

commit 67857e560b03425c5de816b8a62b7e84b6bb2db6

Updated to use Modern Objective-C syntax
Used refactor functionality in XCode with some minor manual changes:
- Fixes to indentation
- Undid several proposed conversions to property, in particular where invocation has side-effect and is not idempotent

Still pending: Use synthesised getter and setter methods where possible and remove custom backing fields.

task/ProgressTracker.m
@@ -11,7 +11,7 @@ NSString  *EstimatedProgressKey = @"estimatedProgress";
 
 @implementation ProgressTracker
 
-- (id) init {
+- (instancetype) init {
   if (self = [super init]) {
     mutex = [[NSLock alloc] init];
     directoryStack = [[NSMutableArray alloc] initWithCapacity: 16];
@@ -67,16 +67,10 @@ NSString  *EstimatedProgressKey = @"estimatedProgress";
   NSDictionary  *dict;
 
   [mutex lock];
-  dict = [NSDictionary dictionaryWithObjectsAndKeys:
-            [NSNumber numberWithUnsignedInteger: numFoldersProcessed],
-            NumFoldersProcessedKey,
-            [NSNumber numberWithUnsignedInteger: numFoldersSkipped],
-            NumFoldersSkippedKey,
-            [[directoryStack lastObject] path],
-            CurrentFolderPathKey,
-            [NSNumber numberWithFloat: [self estimatedProgress]],
-            EstimatedProgressKey,
-            nil];
+  dict = @{NumFoldersProcessedKey: @(numFoldersProcessed),
+           NumFoldersSkippedKey: @(numFoldersSkipped),
+           CurrentFolderPathKey: [directoryStack.lastObject path],
+           EstimatedProgressKey: @([self estimatedProgress])};
   [mutex unlock];
 
   return dict;
@@ -92,7 +86,7 @@ NSString  *EstimatedProgressKey = @"estimatedProgress";
 @implementation ProgressTracker (ProtectedMethods)
 
 - (void) _processingFolder:(DirectoryItem *)dirItem {
-  if ([directoryStack count] == 0) {
+  if (directoryStack.count == 0) {
     // Find the root of the tree
     DirectoryItem  *root = dirItem;
     DirectoryItem  *parent = nil;

commit d5889e770fb5201500d6063851c24f6ad3299726

Fixed crash introduced by changes in 67857e5
Apparently in new syntax values passed to dictionary initialiser cannot be nil.

task/ProgressTracker.m
@@ -69,7 +69,7 @@ NSString  *EstimatedProgressKey = @"estimatedProgress";
   [mutex lock];
   dict = @{NumFoldersProcessedKey: @(numFoldersProcessed),
            NumFoldersSkippedKey: @(numFoldersSkipped),
-           CurrentFolderPathKey: [directoryStack.lastObject path],
+           CurrentFolderPathKey: [directoryStack.lastObject path] ?: @"",
            EstimatedProgressKey: @([self estimatedProgress])};
   [mutex unlock];
 