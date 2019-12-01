amake/GrandPerspective

commit 31f1912fcf265e25a03a90cdcd63169048de9667

Fix times stored when writing scan data to file
For performance was using CoreFoundation data formatter but this ignored the time zone. Maybe this can be fixed by changing the configuration of the formatter, but resorted simply to use the more modern NSDateFormatter. As functionality is rarely used, performance is less importance than simplicity.

This fixes Bug #78.

tree/TreeWriter.m
@@ -101,28 +101,6 @@ NSLocalizedString(@"Failed to write entire buffer.", @"Error message")
 
 @implementation TreeWriter (ProtectedMethods)
 
-+ (CFDateFormatterRef) timeFormatter {
-  static CFDateFormatterRef dateFormatter = NULL;
-
-  if (dateFormatter == NULL) {
-    // Lazily create formatter
-    dateFormatter = CFDateFormatterCreate(kCFAllocatorDefault,
-                                          NULL,
-                                          kCFDateFormatterNoStyle,
-                                          kCFDateFormatterNoStyle);
-    // Fix the format so that output is locale independent.
-    // Was originally "en_GB" locale, but in OS X 10.11 this went from
-    //   dd/MM/yyyy HH:mm
-    // to
-    //   dd/MM/yyyy, HH:mm
-    // which broke parsing saved scans in some situations and also left out
-    // timezone information. We now use a safer representation.
-    CFDateFormatterSetFormat(dateFormatter, (CFStringRef)DateTimeFormat);
-  }
-
-  return dateFormatter;
-}
-
 + (NSDateFormatter *)nsTimeFormatter {
   static NSDateFormatter *timeFmt = nil;
   if (timeFmt == nil) {
@@ -138,9 +116,7 @@ NSLocalizedString(@"Failed to write entire buffer.", @"Error message")
   if (time == 0) {
     return nil;
   } else {
-    return
-    [(NSString *)CFDateFormatterCreateStringWithAbsoluteTime(NULL, [self timeFormatter], time)
-     autorelease];
+    return [[self nsTimeFormatter] stringFromDate: [NSDate dateWithTimeIntervalSince1970: time]];
   }
 }
 

tree/TreeWriter.h
@@ -59,10 +59,6 @@ extern NSString  *DateTimeFormat;
 
 @interface TreeWriter (ProtectedMethods)
 
-/* Formatter used to create (locale-independent) string reprentations for time values.
- */
-+ (CFDateFormatterRef) timeFormatter CF_RETURNS_NOT_RETAINED;
-
 /* Formatter used to create (locale-independent) string reprentations for time values. Has same
  * format as timeFormatter.
  */

util/NSURL.m
@@ -45,7 +45,7 @@
   NSDate  *creationTime;
   NSError  *error = nil;
   
-  [self getResourceValue: &creationTime forKey:NSURLCreationDateKey error: &error];
+  [self getResourceValue: &creationTime forKey: NSURLCreationDateKey error: &error];
   if (error != nil) {
     NSLog(@"Failed to obtain creation time for %@: %@", self, error.description);
     return NO;
@@ -58,7 +58,7 @@
   NSDate  *modificationTime;
   NSError  *error = nil;
   
-  [self getResourceValue: &modificationTime forKey:NSURLContentModificationDateKey error: &error];
+  [self getResourceValue: &modificationTime forKey: NSURLContentModificationDateKey error: &error];
   if (error != nil) {
     NSLog(@"Failed to obtain modification time for %@: %@", self, error.description);
     return NO;
@@ -71,7 +71,7 @@
   NSDate  *accessTime;
   NSError  *error = nil;
   
-  [self getResourceValue: &accessTime forKey:NSURLContentAccessDateKey error: &error];
+  [self getResourceValue: &accessTime forKey: NSURLContentAccessDateKey error: &error];
   if (error != nil) {
     NSLog(@"Failed to obtain access time for %@: %@", self, error.description);
     return NO;

commit 2d1fcf6a1cfa8fc6c08efbb1012ab97feff7c061

Fix to datetime output of TreeWriter
Replaced dateWithTimeIntervalSince1970: by dateWithTimeIntervalSinceReferenceDate:.
This fixes bug introduced in 31f1912.

tree/TreeWriter.m
@@ -116,7 +116,8 @@ NSLocalizedString(@"Failed to write entire buffer.", @"Error message")
   if (time == 0) {
     return nil;
   } else {
-    return [[self nsTimeFormatter] stringFromDate: [NSDate dateWithTimeIntervalSince1970: time]];
+    return [[self nsTimeFormatter] stringFromDate:
+            [NSDate dateWithTimeIntervalSinceReferenceDate: time]];
   }
 }
 